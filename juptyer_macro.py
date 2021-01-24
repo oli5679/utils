2fe4ca608063bf810c81701a386a210db81a3ec5
Oli Cairns <olicairns93@gmail.com>
2021-01-24 19:08:46 +0000

    Create juptyer_macro.py

---
+++ juptyer_macro.py
@@ -0,0 +1,27 @@
+'''
+Macro for jupyter notebooks
+
+Save locally 
+
+    %macro -q import_setup <cell_number>
+    %store import_setup
+    
+Load
+
+    %store -r import_setup
+    
+'''
+
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+import re
+from sklearn.model_selection import train_test_split
+import sklearn
+from tqdm import tqdm
+
+%load_ext lab_black
+
